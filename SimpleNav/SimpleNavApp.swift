f8dcf3442a23fab86bccecf7ac136a067387cd99
Mark Jeschke <markjeschke@yahoo.com>
2023-04-24 12:22:56 -0700

    Initial Commit

---
+++ SimpleNav/SimpleNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavApp.swift
+//  SimpleNav
+//
+//  Created by Mark Jeschke on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
